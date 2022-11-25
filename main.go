748c30d08aca68193578408810b8a09d8a9232c8
Mads Leonardi <mads.leonardi@web.de>
2022-11-25 21:49:06 +0100

    main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"lionauth.ml/goAuth/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init()  {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 	fmt.Println("Hello World")
 }
